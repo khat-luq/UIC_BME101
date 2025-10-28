ed06d5c368302cb51b38ebae55a87f09223fdb17
khat-luq <khattak.luqmaan@gmail.com>
2025-10-28 10:46:44 -0500

    Update BME101_INCLASS2_Luqmaan_Khattak.m

--- BME101_INCLASS2_Luqmaan_Khattak.m
+++ BME101_INCLASS2_Luqmaan_Khattak.m
@@ -1,15 +1,19 @@
 % Image Loading
-
 pepperImage = imread("peppers.png");
 
 % Image Resize
-
 pepperImage = imresize(pepperImage, .5);
 
 % Grayscale
-
 grayImage = rgb2gray(pepperImage);
 
 % Show Image
-
 imshow(pepperImage)
+
+% Display Image
+tiledlayout(1,5)
+
+% Original
+nexttile
+imshow(pepperImage)
+title("Original")
